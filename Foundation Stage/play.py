dc02f10c666b412eb4192ee2c22d253f85d6ed20
郭淑瑛 <2235917772@qq.com>
2021-09-16 09:47:52 +0800

    练习2

--- Foundation Stage/play.py
+++ Foundation Stage/play.py
@@ -4,3 +4,5 @@ Created on Tue Sep 14 11:50:08 2021
 
 @author: 瑛
 """
+
+
---
+++ Foundation Stage/stage1/text0.3.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 10:25:27 2021
+
+@author: 瑛
+"""
+list0=['Tom','Rose']
+list1=[1,2,3,4]
+list0.append("Rose")
+
+print(list0)
+
+list1.insert(1,"韦老师")
+print(list1)
+
+
+
+
+
